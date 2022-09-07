e674e7245a1bbadfb70fceeac448f69fe838d457
pepethefrog <andreas_pat@hotmail.gr>
2022-09-07 16:18:36 +0300

    Moving variables outside loop

--- jacobi_serial-Wtime.c
+++ jacobi_serial-Wtime.c
@@ -84,12 +84,12 @@ double checkSolution(double xStart, double yStart,
 int main(int argc, char **argv)
 {
     /** VALUES ARE ASSIGNED MANUALLY FOR TESTING REASONS.
-     * THIS IS TEMPORARY!
-     */
+    * THIS IS TEMPORARY!
+    */
     int n = 840, m = 840 , mits = 50;
     double alpha = 0.8, tol = 1e-13, relax = 1.0;
     double maxAcceptableError;
-    double error;
+    double error = HUGE_VAL;
     double *u, *u_old, *tmp;
     int allocCount;
     int iterationCount, maxIterationCount;
@@ -142,69 +142,53 @@ int main(int argc, char **argv)
     alpha         = alpha;
 
     iterationCount = 0;
-    error = HUGE_VAL;
     clock_t start = clock(), diff;
+
+    // Variable declarations moved outside the loop
+
+#define SRC(XX,YY) src[(YY)*maxXCount+(XX)]
+#define DST(XX,YY) dst[(YY)*maxXCount+(XX)]
+    double updateVal;
+    double f;
+    // Coefficients
+    double cx = 1.0/(deltaX*deltaX);
+    double cy = 1.0/(deltaY*deltaY);
+    double cc = -2.0*cx-2.0*cy-alpha;
+
+    int x, y;
+    double fX[maxXCount], fY[maxYCount];
+    // Create 2 arrays with fX and fY values. These values are fixed for each iteration.
+    // We are doing this in the same array because always x=y.
+    for (y = 1; y < (maxYCount-1); y++)
+    {
+        fY[y-1] = yStart + (y-1)*deltaY;
+        fX[y-1] = xStart + (y-1)*deltaX;
+    }
 
     MPI_Init(NULL,NULL);
     t1 = MPI_Wtime();
 
-    // Get the number of processes
-    int world_size;
-    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    //Get the rank of the process
-    int world_rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
-
-    printf(".Hello world from processor %s, rank %d out of %d processors\n", processor_name, world_rank, world_size);
     /* Iterate as long as it takes to meet the convergence criterion */
     while (iterationCount < maxIterationCount && error > maxAcceptableError)
     {
-//        printf("Iteration %i", iterationCount);
-
-
-#define SRC(XX,YY) src[(YY)*maxXCount+(XX)]
-#define DST(XX,YY) dst[(YY)*maxXCount+(XX)]
-        int x, y;
-        double fX, fY;
-        double error = 0.0;
-        double updateVal;
-        double f;
-        // Coefficients
-        double cx = 1.0/(deltaX*deltaX);
-        double cy = 1.0/(deltaY*deltaY);
-        double cc = -2.0*cx-2.0*cy-alpha;
-
+        //printf("Iteration %i\n", iterationCount);
+        error = 0.0;
+        // Start of iterations
         for (y = 1; y < (maxYCount-1); y++)
         {
-
+            //fY = yStart + (y-1)*deltaY;
             for (x = 1; x < (maxXCount-1); x++)
             {
-                /***
-                 * Step A.2.b from 'instructions for effective MPI'
-                 * Remove variable assignments (fX, fY). Replace fx, fy in f with their values.
-                 * Did the same below with 'updateVal'.
-                 */
-
-                f = -alpha*(1.0-(xStart + (x-1)*deltaX)*(xStart + (x-1)*deltaX))
-                    *(1.0-(yStart + (y-1)*deltaY)*(yStart + (y-1)*deltaY))
-                    - 2.0*(1.0-(xStart + (x-1)*deltaX)*(xStart + (x-1)*deltaX))
-                    - 2.0*(1.0-(yStart + (y-1)*deltaY)*(yStart + (y-1)*deltaY));
-
+              //  fX = xStart + (x-1)*deltaX;
+                //f = -alpha*(1.0-fX[x]*fX[x])*(1.0-fY[y]*fY[y]) - 2.0*(1.0-fX[x]*fX[x]) - 2.0*(1.0-fY[y]*fY[y]);
+                updateVal = (	(SRC(x-1,y) + SRC(x+1,y))*cx +
+                                 (SRC(x,y-1) + SRC(x,y+1))*cy +
+                                 SRC(x,y)*cc
+                                 -(-alpha*(1.0-fX[x]*fX[x])*(1.0-fY[y]*fY[y]) - 2.0*(1.0-fX[x]*fX[x]) - 2.0*(1.0-fY[y]*fY[y])))
+                                         /cc;
                 DST(x,y) = SRC(x,y) - omega*updateVal;
-                error += ((SRC(x-1,y) + SRC(x+1,y))*cx +
-                          (SRC(x,y-1) + SRC(x,y+1))*cy +
-                          SRC(x,y)*cc - f
-                         )/cc
-                         *
-                         ((SRC(x-1,y) + SRC(x+1,y))*cx +
-                          (SRC(x,y-1) + SRC(x,y+1))*cy +
-                          SRC(x,y)*cc - f
-                         )/cc;
+                error += updateVal*updateVal;
             }
         }
         error= sqrt(error)/((maxXCount-2)*(maxYCount-2));
