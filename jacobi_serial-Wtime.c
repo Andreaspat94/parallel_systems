5ce71af567830f130b19952e359b3818cd592cce
pepethefrog <andreas_pat@hotmail.gr>
2022-09-08 16:44:38 +0300

    Start building the MPI

--- jacobi_serial-Wtime.c
+++ jacobi_serial-Wtime.c
@@ -89,6 +89,7 @@ int main(int argc, char **argv)
     int n = 840, m = 840 , mits = 50;
     double alpha = 0.8, tol = 1e-13, relax = 1.0;
     double maxAcceptableError;
+    /** DONT FORGET THIS */
     double error = HUGE_VAL;
     double *u, *u_old, *tmp;
     int allocCount;
@@ -145,7 +146,7 @@ int main(int argc, char **argv)
     clock_t start = clock(), diff;
 
     // Variable declarations moved outside the loop
-
+    //macros are defined to translate the 2-D index (XX, YY) to the 1-dimensional array:
 #define SRC(XX,YY) src[(YY)*maxXCount+(XX)]
 #define DST(XX,YY) dst[(YY)*maxXCount+(XX)]
     double updateVal;
@@ -165,9 +166,43 @@ int main(int argc, char **argv)
         fX[y-1] = xStart + (y-1)*deltaX;
     }
 
+    MPI_Comm comm;
+    int size, rank, world_size, world_rank, name_len;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+
+    //The array containing the number of processes to assign to each dimension.
+    int dims[2] = {0,0};
+    //logical array of size ndims specifying whether the grid is periodic ( true) or not ( false) in each dimension
+    int period[] = {0,0};
+    // ranking may be reordered (true) or not (false)
+    int reorder = 1;
+    int coords[2];
+
+    /** MPI INIT */
     MPI_Init(NULL,NULL);
     t1 = MPI_Wtime();
 
+    // Get the number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+    // Get the rank of process
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    //This routine decomposes a given number of processes
+    //over a cartesian grid made of the number of dimensions specified.
+    MPI_Dims_create(world_size, 2, dims);
+
+    MPI_Get_processor_name(processor_name, &name_len);
+    printf("Hello from processor %s, rank %d out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    MPI_Cart_create(MPI_COMM_WORLD, 2, dims, period, reorder, &comm);
+
+    MPI_Comm_size(comm, &size);
+    MPI_Comm_rank(comm, &rank);
+
+    //Get my coordinated in the new communicator
+    MPI_Cart_coords(comm, rank, 2, coords);
+    printf("[MPI process %d] I am located at (%d, %d).\n", rank, coords[0],coords[1]);
 
     /* Iterate as long as it takes to meet the convergence criterion */
     while (iterationCount < maxIterationCount && error > maxAcceptableError)
@@ -177,11 +212,8 @@ int main(int argc, char **argv)
         // Start of iterations
         for (y = 1; y < (maxYCount-1); y++)
         {
-            //fY = yStart + (y-1)*deltaY;
             for (x = 1; x < (maxXCount-1); x++)
             {
-              //  fX = xStart + (x-1)*deltaX;
-                //f = -alpha*(1.0-fX[x]*fX[x])*(1.0-fY[y]*fY[y]) - 2.0*(1.0-fX[x]*fX[x]) - 2.0*(1.0-fY[y]*fY[y]);
                 updateVal = (	(SRC(x-1,y) + SRC(x+1,y))*cx +
                                  (SRC(x,y-1) + SRC(x,y+1))*cy +
                                  SRC(x,y)*cc
@@ -202,14 +234,13 @@ int main(int argc, char **argv)
     }
 
     t2 = MPI_Wtime();
-    printf( "Iterations=%3d Elapsed MPI Wall time is %f\n", iterationCount, t2 - t1 );
+    printf( "Rank %d: Iterations=%3d Elapsed MPI Wall time is %f\n", rank, iterationCount, t2 - t1 );
     MPI_Finalize();
 
 
     diff = clock() - start;
     int msec = diff * 1000 / CLOCKS_PER_SEC;
-    printf("Time taken %d seconds %d milliseconds\n", msec/1000, msec%1000);
-    printf("Residual %g\n",error);
+    printf("Rank %d: Time taken %d seconds %d milliseconds\n", rank, msec/1000, msec%1000);
 
     // u_old holds the solution after the most recent buffers swap
     double absoluteError = checkSolution(xLeft, yBottom,
@@ -217,7 +248,11 @@ int main(int argc, char **argv)
                                          u_old,
                                          deltaX, deltaY,
                                          alpha);
-    printf("The error of the iterative solution is %g\n", absoluteError);
+    if (rank == 0) {
+        printf("Residual %g\n", error);
+        printf("The error of the iterative solution is %g\n", absoluteError);
+    }
+
 
     return 0;
 }
